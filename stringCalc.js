3228bb1719c7467444b07dc4a92165d199903741
sakthivelo <123522869+sakthivelo@users.noreply.github.com>
2025-01-06 17:48:12 +0530

    Added more code for adding new  inputs
    File updated for adding string of numbers with comma separated input string.

--- stringCalc.js
+++ stringCalc.js
@@ -1,5 +1,27 @@
 function sumStrNumbers(num){
-    if(!num) return 0;
-}
+    if(!num.length) return 0;
+    
+    let delim = /,/;
+    
+    if (num.startsWith("//")) {
+        const delimiterEnd = num.indexOf("\n");
+        delim = new RegExp(num.substring(2, delimiterEnd));
+        num = num.substring(delimiterEnd + 1);
+    }
+    
+    const numArr = num.split(delim);
 
-console.assert(sumStrNumbers("") === 0, 'Test Failed : "" Should return 0');
+    const sum = numArr.reduce((total, number) => {
+        const parsedNum = parseInt(number);
+        if (isNaN(parsedNum)) return total;
+        
+        return total + parsedNum;
+    }, 0);
+    
+    return sum;
+}
+console.assert(sumStrNumbers("") === 0, 'Test Failed: "" should return 0');
+console.assert(sumStrNumbers("1") === 1, 'Test Failed: "1" should return 1');
+console.assert(sumStrNumbers("1,2") === 3, 'Test Failed: "1,2" should return 3');
+
+
